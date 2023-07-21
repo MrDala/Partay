30f731ab7bfd46dcddcb3e4f38f7dbad2b310478
Alexis <LEON>
2023-07-21 13:21:22 +0200

    Correction fuseau horaire

--- partay-back/sequelize.js
+++ partay-back/sequelize.js
@@ -1,5 +1,10 @@
 const { Sequelize } = require('sequelize');
+const { DateTime } = require('luxon');
+
 require('dotenv').config();
+
+const localDateTime = DateTime.local();
+const databaseTimezoneOffset = localDateTime.toFormat('ZZ');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -8,6 +13,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, pr
   define: {
     timestamps: false
   },
+  timezone: databaseTimezoneOffset
 });
 
 module.exports = sequelize;
